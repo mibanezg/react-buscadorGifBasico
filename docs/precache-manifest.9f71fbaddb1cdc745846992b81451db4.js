4f3573da4be36a940aecdc988ebd2d44243ec2ae
Mario Ibáñez <ibanezg.mario@gmail.com>
2020-09-21 00:29:46 -0300

    Docs agregados

---
+++ docs/precache-manifest.9f71fbaddb1cdc745846992b81451db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ffd234e2ae87e6a0031b2519544ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34322ef2d7d3bd970561",
+    "url": "/static/css/main.3e631ffd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34322ef2d7d3bd970561",
+    "url": "/static/js/main.f5b80a6c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
